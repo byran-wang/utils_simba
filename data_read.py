cc645275800255753be4c326d4596614c6b051bf
simba.wang <simba1986.wang@gmail.com>
2024-07-16 14:50:42 +0800

    add cam_type for readCamerasFromBlenderJson.

--- data_read.py
+++ data_read.py
@@ -20,7 +20,9 @@ class CameraInfo(NamedTuple):
     fov_y: float
     image_name: str
 
-def readCamerasFromBlenderJson(path, transformsfile):
+def readCamerasFromBlenderJson(path, transformsfile, cam_type="cvc2cvw"):
+    # Read the camera information from the transforms.json file
+    cam_type = cam_type.lower() # "cvc2cvw" or "cvc2blw" or "blc2blw"
     cam_infos = []
 
     with open(os.path.join(path, transformsfile)) as json_file:
@@ -31,14 +33,24 @@ def readCamerasFromBlenderJson(path, transformsfile):
         height = meta["h"]
         fov_x = math.atan(width / (2 * fl_y)) * 2
         fov_y = math.atan(height / (2 * fl_y)) * 2
-        gl_to_cv_t = np.array([[1, 0, 0, 0],[0, -1 , 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        cvc2blc = np.array([[1, 0, 0, 0],[0, -1 , 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        blw2cvw = np.array([[0, 1, 0, 0],[0, 0 , -1, 0], [-1, 0, 0, 0], [0, 0, 0, 1]])
         for idx, frame in enumerate(meta["frames"]):
             fname = path / Path(frame["file_path"])
             if not fname:
                 num_skipped_image_filenames += 1
             else:
-                c2w_gl = np.array(frame["transform_matrix"])
-                cam_infos.append(CameraInfo(uid=idx, c2w4x4=c2w_gl, fl_x=fl_x, fl_y=fl_y, image_name=fname, 
+                blc2blw = np.array(frame["transform_matrix"])
+                # c2w_cv = gl_to_cv_t @ c2w_gl
+                if cam_type == "cvc2cvw":
+                    cvc2cvw = blw2cvw @ blc2blw @ cvc2blc
+                    c2w_final = cvc2cvw
+                if cam_type == "cvc2blw":
+                    cvc2blw = blc2blw @ cvc2blc
+                    c2w_final = cvc2blw
+                elif cam_type == "blc2blw":
+                    c2w_final = blc2blw
+                cam_infos.append(CameraInfo(uid=idx, c2w4x4=c2w_final, fl_x=fl_x, fl_y=fl_y, image_name=fname, 
                                             height=height, width=width, fov_x=fov_x, fov_y=fov_y))
         if num_skipped_image_filenames >= 0:
             CONSOLE.print(f"Skipping {num_skipped_image_filenames} files in dataset {path}.")

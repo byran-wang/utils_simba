575c9b1109ebf40cf56bf0cf81892d96ae0efbcd
simba.wang <simba1986.wang@gmail.com>
2024-07-24 10:07:36 +0800

    delete readCamerasFromRealImageWithGTPose

--- data_read.py
+++ data_read.py
@@ -141,64 +141,6 @@ def get_focal_lengths(meta):
 
     return (fl_x, fl_y)
 
-def readCamerasFromRealImageWithGTPose(config):
-    cam_type = config.cam_type.lower() # "cvc2cvw" or "cvc2blw" or "blc2blw"
-    cam_infos = []
-    rgba_all = sorted(glob(f"{config['rgba_path']}/*.png"))
-    pose_all = sorted(glob(f"{config['pose_path']}/*.pkl"))
-    if config.end_frame == -1:
-        end_fname = rgba_all[-1]
-        end = int(op.basename(end_fname).split(".")[0].split("_rgba")[0])
-        config.end_frame = end
-    assert config.end_frame > config.start_frame
-
-    frame_range = list(range(config.start_frame, config.end_frame, config.frame_interval))
-    valid_frames = []
-    
-    for i in frame_range:
-        rgba = os.path.join(config['rgba_path'], f"{i:04d}_rgba.png")
-        if (rgba in rgba_all):
-            pose = os.path.join(config['pose_path'], f"{i:04d}.pkl")
-            if pose in pose_all:
-                if i not in config.exclude_frames:
-                    valid_frames.append([rgba, pose])
-            else:
-                print(f"Warning: {pose} not found")
-        else:
-            print(f"Warning: {rgba} not found")
-    assert len(valid_frames) > 0
-    for ci, [rgba_f, pose_f] in enumerate(valid_frames):
-        meta = pickle.load(open(pose_f,'rb'))
-        if meta['objTrans'] is None or meta['objRot'] is None or meta['camMat'] is None:
-            print(f"Warning: Pose not found for Meta file {pose_f}, and skip frame {rgba_f}")
-            continue
-        if cam_infos == []:
-            K = meta['camMat']
-            fl_x, fl_y = K[0, 0], K[1, 1]
-            cx, cy = K[0, 2], K[1, 2]
-            rgba_imgae = cv2.imread(rgba_f, cv2.IMREAD_UNCHANGED)
-            height, width = rgba_imgae.shape[:2]
-            fov_x = math.atan(width / (2 * fl_x)) * 2
-            fov_y = math.atan(height / (2 * fl_y)) * 2
-            cvw2blw = np.array([[0, 0, -1, 0],[1, 0 , 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
-            blc2glc = np.array([[1, 0, 0, 0],[0, 1 , 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-            glc2cvc = np.array([[1, 0, 0, 0],[0, -1 , 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        cvw2glc = np.eye(4)
-        cvw2glc[:3,3] = meta['objTrans']
-        cvw2glc[:3,:3] = cv2.Rodrigues(meta['objRot'].reshape(3))[0]
-        glc2cvw = np.linalg.inv(cvw2glc)
-        if cam_type == "blc2blw":
-            blc2blw = cvw2blw @ glc2cvw @ blc2glc
-            c2w_final = blc2blw
-        if cam_type == "cvc2cvw":
-            cvw2cvc = glc2cvc @ cvw2glc
-            cvc2cvw = np.linalg.inv(cvw2cvc)
-            c2w_final = cvc2cvw
-        cam_infos.append(CameraInfo(uid=ci, c2w4x4=c2w_final, fl_x=fl_x, fl_y=fl_y, cx=cx, cy=cy, image_name=rgba_f, mask_name=None, pts3d=None,
-                            height=height, width=width, fov_x=fov_x, fov_y=fov_y))
-        
-    return cam_infos
-
 
 def preprocessCamerasFromRealImageWithGtPose(config):
     rgb_all = sorted(glob(f"{config['rgb_path']}/*.jpg"))

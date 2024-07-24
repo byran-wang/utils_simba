5546822e189e5b77396c7e9ae3d8ece68a93777a
simba.wang <simba1986.wang@gmail.com>
2024-07-24 22:03:34 +0800

    add cartesian_to_spherical

--- pose.py
+++ pose.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def RT_opengl2opencv(RT):
      # Build the coordinate transform matrix from world to computer vision camera
@@ -27,3 +28,20 @@ def to_homo(RT):
     RT_h = np.concatenate([RT, np.array([[0,0,0,1]])], axis=0)
     
     return RT_h
+
+def cartesian_to_spherical(xyz, unit="degree"):
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    distance = np.sqrt(xy + xyz[:,2]**2)
+    #theta = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    elevation = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    azimuth = np.arctan2(xyz[:,1], xyz[:,0])
+    if unit == "degree":
+        return {"elevation": elevation.item() * 180 / math.pi,
+                "azimuth":  azimuth.item() * 180 / math.pi,
+                "distance": distance.item(),
+                }
+    else:
+        return {"elevation": elevation.item(),
+                "azimuth":  azimuth.item(),
+                "distance": distance.item(),
+                }

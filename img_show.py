6f4f279af48b23b3ca733ff57c6e764895d97324
simba.wang <simba1986.wang@gmail.com>
2024-09-14 20:13:43 +0800

    add read_image and read_mask in img_show.py

--- img_show.py
+++ img_show.py
@@ -1,5 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 # image shape should be [height, width, channels] and in range [0, 1]
 def show_img(img, normalize=False, title=None, off_axis=True):
@@ -19,3 +21,26 @@ def show_img(img, normalize=False, title=None, off_axis=True):
         plt.gca().xaxis.set_major_locator(plt.NullLocator())  # Remove x-axis ticks
         plt.gca().yaxis.set_major_locator(plt.NullLocator())  # Remove y-axis ticks
     plt.show()
+
+
+def read_image(path, grayscale=False):
+    if grayscale:
+        mode = cv2.IMREAD_GRAYSCALE
+    else:
+        mode = cv2.IMREAD_COLOR
+    image = cv2.imread(str(path), mode)
+    if image is None:
+        raise ValueError(f"Cannot read image {path}.")
+    if not grayscale and len(image.shape) == 3:
+        image = image[:, :, ::-1]  # BGR to RGB
+    return image
+
+def read_mask(path):
+    image = cv2.imread(str(path), cv2.IMREAD_UNCHANGED)
+    if image.shape[2] == 4:
+        mask = image[:, :, 3]
+        _, binary_mask = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)
+        mask = binary_mask / 255
+    else:
+        mask = np.ones_like(image[:, :, 0])
+    return mask
